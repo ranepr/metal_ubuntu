dcdcf01fc3d9382f6c467ab1bcc68e77ab65efca
Pramod Rane <46024859+ranepr@users.noreply.github.com>
2020-12-11 00:05:49 +0800

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,16 @@
+variable "token" {
+  type = string
+  default = "7wjC8Q4HNbgCxqJSAcqUZCFdkDdefJmG"
+}
+variable "client_id" {
+  type = string
+  default = "prane@ap.equinix.com"
+}
+variable "client_secret" {
+  type = string
+  default = "D3ll3mc@121"
+}
+variable "equinix_project_id" {
+  type = string
+  default = "af5b81d0-679d-4ef7-8106-e3d4ec36fb04"
+}
