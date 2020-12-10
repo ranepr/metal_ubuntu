8b98eec0f203e2f1ed1f5ee4606331cca6dd38b5
Pramod Rane <46024859+ranepr@users.noreply.github.com>
2020-12-11 00:07:18 +0800

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,16 +1,16 @@
 variable "token" {
   type = string
-  default = "7wjC8Q4HNbgCxqJSAcqUZCFdkDdefJmG"
+  default = "your token"
 }
 variable "client_id" {
   type = string
-  default = "prane@ap.equinix.com"
+  default = "your client ID"
 }
 variable "client_secret" {
   type = string
-  default = "D3ll3mc@121"
+  default = "You Secret"
 }
 variable "equinix_project_id" {
   type = string
-  default = "af5b81d0-679d-4ef7-8106-e3d4ec36fb04"
+  default = "Your project id"
 }
